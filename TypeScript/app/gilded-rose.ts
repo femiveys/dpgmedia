674a7c584c2950f7e90bf191fb66f6e11021dfad
Femi Veys <femi@itsimplyworks.be>
2020-10-22 20:06:26 +0200

    refactor existing code

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -1,3 +1,8 @@
+const BRIE = "Aged Brie";
+const BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
+const SULFURAS = "Sulfuras, Hand of Ragnaros";
+const CONJURED = "Conjured Mana Cake";
+
 export class Item {
   name: string;
   sellIn: number;
@@ -8,6 +13,30 @@ export class Item {
     this.sellIn = sellIn;
     this.quality = quality;
   }
+
+  shouldHaveBeenSold = () => this.sellIn < 0;
+
+  incrementQuality = () => {
+    // The Quality of an item is never more than 50
+    if (this.quality < 50) {
+      this.quality = this.quality + 1;
+    }
+  };
+
+  decrementQuality = () => {
+    // The Quality of an item is never negative
+    if (this.quality > 0) {
+      this.quality = this.quality - 1;
+    }
+  };
+
+  resetQuality = () => {
+    this.quality = 0;
+  };
+
+  decrementSellIn = () => {
+    this.sellIn = this.sellIn - 1;
+  };
 }
 
 export class GildedRose {
@@ -17,61 +46,63 @@ export class GildedRose {
     this.items = items;
   }
 
-  updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (
-        this.items[i].name != "Aged Brie" &&
-        this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
-      ) {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (
-            this.items[i].name == "Backstage passes to a TAFKAL80ETC concert"
-          ) {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != "Aged Brie") {
-          if (
-            this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
-          ) {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality =
-              this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
-      }
-    }
+  calculateNextDay = () => {
+    this.items
+      .filter((item) => item.name !== SULFURAS)
+      .forEach((item) => {
+        GildedRose.updateItemSellIn(item);
+        GildedRose.updateItemQuality(item);
+      });
 
     return this.items;
-  }
+  };
+
+  static updateItemSellIn = (item: Item) => {
+    item.decrementSellIn();
+  };
+
+  static updateItemQuality = (item: Item) => {
+    switch (item.name) {
+      case BRIE:
+        GildedRose.updateBrieQuality(item);
+        break;
+
+      case BACKSTAGE:
+        GildedRose.updateBackstageQuality(item);
+        break;
+
+      default:
+        GildedRose.updateDefaultQuality(item);
+    }
+  };
+
+  static updateBrieQuality = (item: Item) => {
+    item.incrementQuality();
+    if (item.shouldHaveBeenSold()) {
+      item.incrementQuality();
+    }
+  };
+
+  static updateBackstageQuality = (item: Item) => {
+    item.incrementQuality();
+    if (item.sellIn < 10) {
+      item.incrementQuality();
+    }
+    if (item.sellIn < 5) {
+      item.incrementQuality();
+    }
+    if (item.shouldHaveBeenSold()) {
+      item.resetQuality();
+    }
+  };
+
+  static updateDefaultQuality = (item: Item) => {
+    item.decrementQuality();
+    if (item.shouldHaveBeenSold()) {
+      item.decrementQuality();
+    }
+  };
+
+  // For backward compatibility
+  updateQuality = this.calculateNextDay;
 }
